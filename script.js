b75da053807351e4beef6aa7eaf580422bb0cb97
oKenzz <kenz.phu@gmail.com>
2024-06-23 19:13:47 +0200

    eventListener for add and remove, is valid logic start

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const map = [];
 const rowSets = [];
 const columnSets = [];
 const gridSets = [];
-const ratioNumber = [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-const cells = document.getElementsByClassName('inner-grid')
+const ratioNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let cells = [];
+let emptyCells = 81;
+let selected = null;
 
 const createGrid = () => {
   const numberOfElements = 9;
@@ -15,10 +17,26 @@ const createGrid = () => {
     for (let j = 0; j < numberOfElements; j++) {
       const newInnerGrid = document.createElement("div");
       newInnerGrid.classList.add("inner-grid");
+      newInnerGrid.addEventListener("click", (e) => select(e.target, e));
       newGrid.appendChild(newInnerGrid);
     }
   }
+  document.addEventListener('keydown', (e) => addNumber(selected, Number(e.key)));
+  document.addEventListener('keydown', (e) => removeNumber(selected, e))
 };
+
+const select = (cell, e) => {
+  if (selected == cell) {
+    selected = null;
+    e.target.style.backgroundColor = "white"
+  } else {
+    if (selected != null) {
+      selected.style.backgroundColor = "white"
+    }
+    selected = cell;
+    e.target.style.backgroundColor = "CornFlowerBlue"
+  }
+}
 
 const renderGrid = () => {
   for (let i = 0; i < map.length; i++) {
@@ -38,14 +56,14 @@ const initializeMap = () => {
     rowSets.push(new Set());
     columnSets.push(new Set());
     gridSets.push(new Set());
+    cells = [...document.getElementsByClassName('inner-grid')]
   }
 };
 
 const generateMap = () => {
   for (let i = 0; i < 9; i++) {
     for (let j = 0; j < 9; j++) {
-      const index = Math.floor(Math.random() * 18);
-      const number = ratioNumber[index];
+      const number = Math.floor(Math.random() * 9);
       const cellNumber = getCellFromRowAndColumn(i, j);
       const gridNumber = getGrid(cellNumber);
       if (number != 0 && !(rowSets.at(i).has(number) || columnSets.at(j).has(number) || gridSets.at(gridNumber).has(number))) {
@@ -53,12 +71,14 @@ const generateMap = () => {
         rowSets[i].add(number);
         columnSets[j].add(number);
         gridSets[gridNumber].add(number);
+        emptyCells -= 1;
       }
     }
   }
   renderGrid();
 };
 
+// Unecessary? just use indexOf
 const getCellFromRowAndColumn = (row, column) => {
   const subgridRowIndex = Math.floor(row / 3);
   const subgridColumnIndex = Math.floor(column / 3);
@@ -83,27 +103,82 @@ const getGrid = (cell) => {
   return Math.floor(cell / 9);
 };
 
-// TODO: Add error handling for unvalid inputs
-const addNumber = (index, number) => {
-  const cell = cells[index];
-  cell.textContent = number
-  const coordinates = getRowAndColumnFromCell(index);
-  const row = coordinates.row;
-  const column = coordinates.column;
-  map[row][column] = number
+const addNumber = (cell, number) => {
+  if (cell != null && number > 0) {
+    cell.textContent = number
+    const coordinates = getRowAndColumnFromCell(cells.indexOf(cell));
+    const row = coordinates.row;
+    const column = coordinates.column;
+    if (map[row][column] == 0) {
+      emptyCells -= 1;
+    }
+    map[row][column] = number;
+    rowSets[row].add(number);
+    columnSets[column].add(number);
+    gridSets[getGrid(cells.indexOf(cell))].add(number);
+    if (emptyCells == 0) {
+      if (isValid()) {
+        alert("Well Done!");
+      } else {
+        alert("Not a valid sudoku!");
+      }
+    }
+  }
 };
 
-// TODO: Add error handling for unvalid inputs
-const removeNumber = (index) => {
-  const cell = cells[index];
-  cell.textContent = "";
-  const coordinates = getRowAndColumnFromCell(index);
-  const row = coordinates.row;
-  const column = coordinates.column;
-  map[row][column] = 0
-};
+const removeNumber = (cell, e) => {
+  if (e.keyCode == 8) {
+    cell.textContent = "";
+    const coordinates = getRowAndColumnFromCell(cells.indexOf(cell));
+    const row = coordinates.row;
+    const column = coordinates.column;
+    const number = map[row][column];
+    map[row][column] = 0;
+    rowSets[row].delete(number);
+    columnSets[column].delete(number);
+    gridSets[getGrid(cells.indexOf(cell))].delete(number);
+    emptyCells += 1;
+  };
+}
 
-createGrid();
-initializeMap();
-generateMap();
-console.log(cells)
+const isValid = () => {
+  for (let i = 0; i < 9; i++) {
+    for (let j = 1; j < 9; j++) {
+      if (!(gridSets[i].has(j) && rowSets[i].has(j) && columnSets[i].has(j))) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+const solveSudoku = () => {
+  // TODO:
+}
+
+const initialize = () => {
+  createGrid();
+  initializeMap()
+  generateMap();
+  console.log(cells)
+}
+
+// TODO:
+//  1. Change createGrid() to also intialize the map with number so eventListener is only applied to empty cells
+//  2. Lock in system mode to test (toggleable)
+//  3. When everything is filled => isValid()
+//  4. Handle proper addition and deletion of sets values,
+//  5. Implement solver, backtracking algorithm
+//
+// Ideas:
+//  - Selection, Have to lock in so you can try different numbers and follow through and determine if it makes sense
+//  - paper (Notes)
+//  - Remaining Number left to be placed
+//  - Highlight all number of selected
+//  - Multiple input method for numbers
+//  - Highlight wrong in current context
+
+// Problems:
+// - Removing all cells lead to incosistent cell sizes
+// - Uncaught error caused by removeNumber
+initialize()
